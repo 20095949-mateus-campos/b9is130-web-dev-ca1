37e9cfdcf94a8f6e4aa55d3410d76884863be062
20095949-mateus-campos <20095949@mydbs.ie>
2026-04-20 17:34:09 +0100

    added initial integration with discogs api

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,42 @@
-from fastapi import FastAPI
-import requests
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+from typing import List
+import models
+import schemas
 
-app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Music Record Store API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Music Record Store API"}
+    return {"message": f"Welcome to the {app.title}"}
+
+@app.get("/records", response_model=List[schemas.RecordSchema])
+def get_records(db: Session = Depends(get_db)):
+    records = db.query(models.Record).all()
+    return records
 
 @app.get("/records/{record_id}")
 def get_record(record_id: int):
-
     return {
         "id": record_id,
         "title": "Sample Record",
